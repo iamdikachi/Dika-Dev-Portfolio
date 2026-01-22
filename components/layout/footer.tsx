e50d68fa7a74b50696d74561abfd424ebb90f152
Emmanuelnnadi111 <doneonyi920@gmail.com>
2026-01-22 11:25:51 +0100

    added extra padding to the footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,7 +4,7 @@ import { SocialLinks } from "../ui/socialLinks";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-dark-bg text-gray-400 py-3 px-6 md:px-10 border-t border-teal-400/5">
+    <footer className="bg-dark-bg text-gray-400 pt-6 pb-42 md:py-6 px-6 md:px-10 border-t border-teal-400/5">
       <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
         <p className="text-sm md:text-base">
           © 2025 Onyedikachi Emmanuel. All rights reserved.
